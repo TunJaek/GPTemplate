18c64f80cce6616f12463bbbe807b7c588a277e3
TunJaek <tasan1605@naver.com>
2023-09-05 16:33:43 +0900

    initial commit

---
+++ GPTemplate/src/test/java/com/milkeat/GPTemplate/GpTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milkeat.GPTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
